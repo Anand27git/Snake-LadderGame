b15db4c9e1f6d322f076ce06ea9aafbb73d12306
Anand27git <anandkumar256k@gmail.com>
2021-05-25 21:54:00 +0530

    Rolling Dice 1to6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,6 +4,10 @@ public static void main(String[] args)
 {
 System.out.println("Welcome to the SnakeLadder Game");
 
+
 	int startPostion=0;
+	int diceroll = (int) (Math.random()*6)+1;
+	
+	System.out.println(" Dice is : "+ diceroll);
 }
 }
