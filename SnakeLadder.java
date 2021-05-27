06f54abd03b4d1ac0dc01dc57d774700a8d69ee3
Anand27git <anandkumar256k@gmail.com>
2021-05-27 18:54:57 +0530

    added Winning Position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,13 +4,15 @@ public class SnakeLadder {
 	public static final int LADDER = 1 ;
 	public static final int SNAKE = 2 ;
 
-public static void main(String[] args)
-{
-System.out.println("Welcome to the SnakeLadder Game");
+	public static void main(String[] args) {
+	System.out.println("Welcome to the SnakeLadder Game");
 
 
 	int position=0;
 	int newPosition=0;
+	int winPosition=100;
+
+	while (position<=winPosition) {
 	int diceroll = (int) (Math.random()*6)+1;
 	int checkOption =(int) (Math.random()*3);
 	System.out.println(" Dice is : "+ diceroll);
@@ -32,6 +34,6 @@ System.out.println("Welcome to the SnakeLadder Game");
 		position = 0;
 		 }
 	System.out.println("Your are now at "+position+"th"+position);
+	}
+     }
 }
-}
-
