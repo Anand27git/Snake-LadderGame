e310e4703fff1f644d76ca735503096698d7b0c6
Anand27git <anandkumar256k@gmail.com>
2021-05-27 20:28:09 +0530

    added TwoPlayers

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,38 +6,93 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 	System.out.println("Welcome to the SnakeLadder Game");
-
-
-	int position=0;
+	
+	int player1_position=0;
+	int player2_position=0;
 	int newPosition=0;
 	int winPosition=100;
-	int count=0;
-	while (position<=winPosition) {
+	int count1=0;
+	int count2=0;
+	int currentPlayer=1;
+	int checkOption;
+	while (true) {
+		if(currentPlayer==1) {
+		System.out.println("now player-1's turn");
+		count1++;
 	int diceroll = (int) (Math.random()*6)+1;
-	int checkOption =(int) (Math.random()*3);
+	checkOption =(int) (Math.random()*3);
 	System.out.println(" Dice is : "+ diceroll);
-	count++;
-	switch(checkOption)
-	{
+
+	switch(checkOption) {
 	case NO_PLAY :
 		newPosition = 0;
 		break;
 	case LADDER :
-		newPosition =diceroll;
+		newPosition = diceroll;
 		break;
 	case SNAKE :
 		newPosition = -diceroll;
 		break;
 	}
-	position=position+newPosition;
-	if (position<0) {
-		position = 0;
+	player1_position=player1_position+newPosition;
+	if (player1_position<0) {
+		player1_position = 0;
 		 }
-	if (position>100){
-	position=newPosition;
+	if (player1_position>100){
+	player1_position=newPosition;
 	}
-	System.out.println("Your are now at "+position+"th"+position);
+	System.out.println("now Player-1 at"+player1_position+"th"+player1_position);
+	if(player1_position==100){
+		break;
 	}
-	System.out.println("\n Wow you win!! \n after dice rolled" +count+"times.");
-     }
+	}
+	else {
+		count2++;
+		System.out.println("now player-2's turn");
+		int diceroll =(int) (Math.random()*6)+1;
+		checkOption =(int) (Math.random()*3);
+
+	switch(checkOption) {
+
+	        case NO_PLAY :
+                newPosition = 0;
+                	break;
+        	case LADDER :
+                newPosition = diceroll;
+                	break;
+        	case SNAKE :
+                newPosition = -diceroll;
+                	break;
+        }
+        player2_position=player2_position+newPosition;
+        if (player2_position<0) {
+                player2_position = 0;
+                 }
+        if (player2_position>100){
+        player2_position=newPosition;
+        }
+        System.out.println("now Player-2 at"+player2_position+"th"+player2_position);
+	if(player2_position==100){
+		break; 
+	}
+}
+
+	if(checkOption==1) {
+	System.out.println("ohh, You got ladder. Now play Again");
+	}
+	else {
+		if(currentPlayer==1) {
+		currentPlayer = 2;
+		}
+		else {
+		currentPlayer = 1;
+		}
+	}
+}
+	if(player1_position==100) {
+	System.out.println("\n Congratulation Player-1 you are win! \n after dice rolled " + count1+"times.");
+	} else {
+	System.out.println("\n Congratulation player-2 you are win! \n after dice rolled" +count2+" times." );
+	}
+    }
 }
